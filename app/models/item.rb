932d62ac249da71dda0f5ddb4d9791b60f55891c
RyuseiNono <ryuno72444za@gmail.com>
2020-12-04 17:02:39 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  # アソシエーションを定義
+  belongs_to :user
   has_one_attached :image
 
   # アクティブハッシュのアソシエーションを定義
@@ -9,14 +11,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
-  #空の投稿を保存できないようにする
+  # 空の投稿を保存できないようにする
   with_options presence: true do
     validates :image
-    validates :name
-    validates :information
+    validates :name, length: { maximum: 40 }
+    validates :information, length: { maximum: 1000 }
 
-    #ジャンルの選択が「--」の時は保存できないようにする
-    with_options  numericality: { other_than: 1 } do
+    # ジャンルの選択が「--」の時は保存できないようにする
+    with_options numericality: { other_than: 1 } do
       validates :status_id
       validates :category_id
       validates :shipping_cost_id
@@ -24,9 +26,7 @@ class Item < ApplicationRecord
       validates :days_to_ship_id
     end
 
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     # ¥300~¥9,999,999
   end
-
-
 end

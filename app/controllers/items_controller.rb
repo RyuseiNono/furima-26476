64afe0cceb40b5cac139e675fd0470264f0827d2
RyuseiNono <ryuno72444za@gmail.com>
2020-12-04 17:07:41 +0900

    Rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -10,7 +11,6 @@ class ItemsController < ApplicationController
     @shipping_costs = ShippingCost.all
     @prefectures = Prefecture.all
     @days_to_ships = DaysToShip.all
-
   end
 
   def create
@@ -27,9 +27,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
   private
+
   def item_params
-    params.require(:item).permit(:image,:name,:information,:status_id,:category_id,:shipping_cost_id,:prefecture_id,:days_to_ship_id,:price).merge(user_id: current_user[:id])
+    params.require(:item).permit(:image, :name, :information, :status_id, :category_id, :shipping_cost_id, :prefecture_id, \
+                                 :days_to_ship_id, :price).merge(user_id: current_user[:id])
   end
 end

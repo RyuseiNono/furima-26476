ae4938cf9ff2d38692e690de6d87a5245170356c
RyuseiNono <ryuno72444za@gmail.com>
2020-12-07 21:59:52 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,10 +21,22 @@ class ItemsController < ApplicationController
     @item = Item.includes(:user).find(params[:id])
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    user_can_edit?
+    @item.destroy
+    redirect_to action: :index
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :information, :status_id, :category_id, :shipping_cost_id, :prefecture_id, \
                                  :days_to_ship_id, :price).merge(user_id: current_user[:id])
+  end
+
+  # 編集権限がない場合、詳細ページへリダイレクトする
+  def user_can_edit?
+    redirect_to action: :index unless @item.user_id == current_user.id
   end
 end

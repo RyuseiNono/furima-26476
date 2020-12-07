eccae0bdf0af019f903ad3db1d117d2eda984d61
RyuseiNono <ryuno72444za@gmail.com>
2020-12-07 21:45:26 +0900

    販売金額の範囲外の時を手数料計算から除外

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,7 +4,11 @@ function method() {
   const profit = document.getElementById("profit");
 
   priceInput.addEventListener("input", () => {
-    if (Number.isInteger(Number(priceInput.value))) {
+    if (
+      Number.isInteger(Number(priceInput.value)) &&
+      Number(priceInput.value) >= 300 &&
+      Number(priceInput.value) <= 9999999
+    ) {
       const price = priceInput.value;
       addTaxDom.innerHTML = Math.floor(price * 0.1);
       profit.innerHTML = price - Math.floor(price * 0.1);

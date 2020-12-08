b50db073fa445c7b02edadc270138141fc1fb051
RyuseiNono <ryuno72444za@gmail.com>
2020-12-08 12:33:04 +0900

    rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :destroy] #順番重要
-  before_action :user_can_edit?, only: [:destroy] #順番重要
+  before_action :set_item, only: [:show, :destroy] # 順番重要
+  before_action :user_can_edit?, only: [:destroy] # 順番重要
   def index
     @items = Item.order('created_at DESC')
   end

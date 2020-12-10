1b8a3e5cbb8f89fbb8998f7d66f2d532b47ae3f1
RyuseiNono <ryuno72444za@gmail.com>
2020-12-10 14:27:08 +0900

    書式変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,11 +2,7 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :prefecture_id, :city, :street_address, :building, :phone_number, :item_id, :user_id
 
-  # バリデーションの処理
-  # Orderモデルのバリデーション
-  validates :token, presence: true
 
-  # Addressモデルのバリデーション
   # 空の投稿を保存できないようにする
   with_options presence: true do
     validates :postal_code,     format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
@@ -15,11 +11,13 @@ class OrderAddress
     validates :street_address
     # validates :building #空でも可
     validates :phone_number, numericality: {}
+    validates :token
   end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address,\
                    building: building, phone_number: phone_number, order_id: order.id)
-  end
+
+                  end
 end

887c65f68520c6e1d62de401025c84c00813f714
RyuseiNono <ryuno72444za@gmail.com>
2020-12-10 12:52:22 +0900

    order_addressモデルのFactoryBotを定義

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code       { format('%03d', rand(1000)) + "-" + format('%04d', rand(10000)) }
+    prefecture_id     { Faker::Number.between(from: 2, to: Prefecture.all.length) }
+    city              { Faker::Address.city }
+    street_address    { Faker::Address.street_name }
+    building          { Faker::Address.secondary_address }
+    phone_number      { Faker::Number.leading_zero_number(digits: 10) }
+    token             { "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" }
+  end
+end

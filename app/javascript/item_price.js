02f4ef09efce502d93db907e1a2c437b827322f0
RyuseiNono <ryuno72444za@gmail.com>
2020-12-16 14:51:13 +0900

    画像レビュー実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,22 +1,24 @@
 function method() {
-  const priceInput = document.getElementById("item-price");
-  const addTaxDom = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
+  if (document.URL.match(/items/) && document.URL.match(/new/)) {
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
 
-  priceInput.addEventListener("input", () => {
-    if (
-      Number.isInteger(Number(priceInput.value)) &&
-      Number(priceInput.value) >= 300 &&
-      Number(priceInput.value) <= 9999999
-    ) {
-      const price = priceInput.value;
-      addTaxDom.innerHTML = Math.floor(price * 0.1);
-      profit.innerHTML = price - Math.floor(price * 0.1);
-    } else {
-      addTaxDom.innerHTML = "";
-      profit.innerHTML = "";
-    }
-  });
+    priceInput.addEventListener("input", () => {
+      if (
+        Number.isInteger(Number(priceInput.value)) &&
+        Number(priceInput.value) >= 300 &&
+        Number(priceInput.value) <= 9999999
+      ) {
+        const price = priceInput.value;
+        addTaxDom.innerHTML = Math.floor(price * 0.1);
+        profit.innerHTML = price - Math.floor(price * 0.1);
+      } else {
+        addTaxDom.innerHTML = "";
+        profit.innerHTML = "";
+      }
+    });
+  }
 }
 
 window.addEventListener("load", method);

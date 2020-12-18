674576fc19ac28fc723a196b6e072c348781d3af
RyuseiNono <ryuno72444za@gmail.com>
2020-12-18 19:28:28 +0900

    Revert "seeds"
    This reverts commit e511574fdf0d2ebfab524028d692d95246ee40f6.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,7 @@
-
-num = 5 #作成するアカウント数
-
-num.times do |n|
-  user = Gimei.name
-  password = "123qwe"
-  # password = Faker::Internet.password(min_length: 6) + rand(10).to_s
-  test = User.create(
-    nickname:               Faker::Name.initials(number: 5) ,
-    email:                  "test#{n}@com",
-    password:               password ,
-    password_confirmation:  password ,
-    first_name:             user.first.kanji  ,
-    last_name:              user.last.kanji  ,
-    first_name_kana:        user.first.katakana ,
-    last_name_kana:         user.last.katakana ,
-    birthday:               Faker::Date.birthday(min_age: 18, max_age: 65)
-  )
-end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)

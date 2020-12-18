c38786cdff990efe30a84bc13d912b51315b7e57
RyuseiNono <ryuno72444za@gmail.com>
2020-12-18 19:28:21 +0900

    Revert "メッセージ関係"
    This reverts commit ab865754615d6b023e8dae7fc1979590dd81a4d0.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable#, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         :recoverable, :rememberable, :validatable
 
   has_many :items, dependent: :destroy
   has_many :orders
-  has_many :messages
 
   with_options presence: true do
     validates :nickname
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/new'
-  devise_for :users#, controllers: {
-    # omniauth_callbacks: 'users/omniauth_callbacks',
-    # registrations: 'users/registrations'
-  # }
+  devise_for :users
   root to: "items#index"
   resources :items do
-    resources :orders, only: [:index, :create]
-    resources :messages, only: [:create]
+    resources :orders, only: [:index, :create] do
+    end
   end
 end
